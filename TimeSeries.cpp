6d3c52528ef7ec95bf8f0e4f9816d2e8b8e9d663
Gabriel Mihalache <mihalache@gmail.com>
2020-10-04 01:45:21 -0400

    Added cutTails option to discretize Normal by dropping mass in tails rather than assigning it to end of grid.

--- TimeSeries.cpp
+++ TimeSeries.cpp
@@ -1,9 +1,11 @@
-#include "TimeSeries.hpp"
+#define ARMA_NO_DEBUG
 
 #include <armadillo>
 #include <cmath>
 #include <iostream>
 #include <string>
+
+#include "TimeSeries.hpp"
 
 using namespace arma;
 
@@ -12,43 +14,51 @@ namespace TimeSeries {
 /*
  * Discretize Normal for given grid.
  */
-vec discretizeNormal(vec grid, double mean, double sigma)
+vec discretizeNormal(vec grid, const double mean, const double sigma, bool cutTails)
 {
-    uword sz = grid.n_elem;
-    vec prs(sz);
+  uword sz = grid.n_elem;
+  vec prs(sz);
 
-    if (sz == 1) {
-        prs(0) = 1.0;
-        return prs;
-    } else if (sz == 2) {
-        prs(0) = normcdf((grid(0) + grid(1)) / 2.0, mean, sigma);
-        prs(1) = 1.0 - prs(0);
-        return prs;
-    }
+  if (sz == 1) {
+    prs(0) = 1.0;
+    return prs;
+  } else if (sz == 2) {
+    prs(0) = normcdf((grid(0) + grid(1)) / 2.0, mean, sigma);
+    prs(1) = 1.0 - prs(0);
+    return prs;
+  }
 
+  for (uword ix = 1; ix < sz - 1; ++ix) {
+    prs(ix) = normcdf((grid(ix) + grid(ix + 1)) / 2.0, mean, sigma) - normcdf((grid(ix - 1) + grid(ix)) / 2.0, mean, sigma);
+  }
+
+  if (cutTails) {
+    const double stepOut = (grid(1) - grid(0)) / 2.0;
+    prs(0) = normcdf(grid(0) + stepOut, mean, sigma) - normcdf(grid(0) - stepOut, mean, sigma);
+    prs(sz - 1) = normcdf(grid(sz - 1) + stepOut, mean, sigma) - normcdf(grid(sz - 1) - stepOut, mean, sigma);
+
+    prs /= sum(prs);
+  } else {
     prs(0) = normcdf((grid(0) + grid(1)) / 2.0, mean, sigma);
     prs(sz - 1) = 1.0 - normcdf((grid(sz - 2) + grid(sz - 1)) / 2.0, mean, sigma);
+  }
 
-    for (uword ix = 1; ix < sz - 1; ++ix) {
-        prs(ix) = normcdf((grid(ix) + grid(ix + 1)) / 2.0, mean, sigma) - normcdf((grid(ix - 1) + grid(ix)) / 2.0, mean, sigma);
-    }
-
-    return prs;
+  return prs;
 }
 
 /*
  * Discretize Normal over uniformly spaced grid 
  * spanning +/- pmSigma standard deviations.
  */
-DiscreteRV discretizeNormal(double mean,
-    double sigma,
-    uword n_elem,
-    uword pmSigma)
+DiscreteRV discretizeNormal(const double mean,
+    const double sigma,
+    const uword n_elem,
+    const uword pmSigma, bool cutTails)
 {
-    DiscreteRV drv;
-    drv.support = linspace<vec>(mean - pmSigma * sigma, mean + pmSigma * sigma, n_elem);
-    drv.probabilities = discretizeNormal(drv.support, mean, sigma);
-    return drv;
+  DiscreteRV drv;
+  drv.support = linspace<vec>(mean - pmSigma * sigma, mean + pmSigma * sigma, n_elem);
+  drv.probabilities = discretizeNormal(drv.support, mean, sigma, cutTails);
+  return drv;
 }
 
 /*
@@ -56,247 +66,250 @@ DiscreteRV discretizeNormal(double mean,
  */
 const MvNormal VAR::conditional(vec& current) const
 {
-    MvNormal mvn;
-    mvn.mean = m_intercept + m_rho * current;
-    mvn.varCovar = m_sigma;
-    return mvn;
+  MvNormal mvn;
+  mvn.mean = m_intercept + m_rho * current;
+  mvn.varCovar = m_sigma;
+  return mvn;
 }
 
 void VAR::findStationary()
 {
-    const mat eyeRho = eye(m_size, m_size) - m_rho;
-    double theDet = det(eyeRho);
+  const mat eyeRho = eye(m_size, m_size) - m_rho;
+  double theDet = det(eyeRho);
 
-    if (fabs(theDet) < TStol) {
-        cout << "Determinant " << det(eyeRho) << endl;
-        is_stationary = false;
-        m_statMean.reset();
-        m_statSigma.reset();
-        return;
-    } else
-        is_stationary = true;
+  if (fabs(theDet) < TStol) {
+    cout << "Determinant " << det(eyeRho) << endl;
+    is_stationary = false;
+    m_statMean.reset();
+    m_statSigma.reset();
+    return;
+  } else
+    is_stationary = true;
 
-    m_statMean = solve(eyeRho, m_intercept);
+  m_statMean = solve(eyeRho, m_intercept);
 
-    mat v0 = m_sigma;
-    mat v1 = m_sigma;
-    v0.fill(0.0);
-    double err = 1.0;
-    while (err > TStol) {
-        v1 = m_rho * v0 * m_rho.t() + m_sigma;
-        err = abs(v1 - v0).max();
-        v0 = v1;
-    }
-    m_statSigma = v1;
+  mat v0 = m_sigma;
+  mat v1 = m_sigma;
+  v0.fill(0.0);
+  double err = 1.0;
+  while (err > TStol) {
+    v1 = m_rho * v0 * m_rho.t() + m_sigma;
+    err = abs(v1 - v0).max();
+    v0 = v1;
+  }
+  m_statSigma = v1;
 }
 
 vec& VAR::stationaryMean()
 {
-    if (m_statMean.is_empty())
-        findStationary();
-    return m_statMean;
+  if (m_statMean.is_empty())
+    findStationary();
+  return m_statMean;
 }
 
 mat& VAR::stationarySigma()
 {
-    if (m_statSigma.is_empty())
-        findStationary();
-    return m_statSigma;
+  if (m_statSigma.is_empty())
+    findStationary();
+  return m_statSigma;
 }
 
 bool VAR::stationaryQ()
 {
-    if (m_statMean.is_empty())
-        findStationary();
-    return is_stationary;
+  if (m_statMean.is_empty())
+    findStationary();
+  return is_stationary;
 }
 
 void VAR::print()
 {
-    cout << "Intercept: " << endl;
-    intercept().print();
-    cout << "Rho: " << endl;
-    rho().print();
-    cout << "Sigma: " << endl;
-    sigma().print();
+  cout << "Intercept: " << endl;
+  intercept().print();
+  cout << "Rho: " << endl;
+  rho().print();
+  cout << "Sigma: " << endl;
+  sigma().print();
 
-    cout << "Stationary: " << endl;
-    stationaryMean().print();
-    cout << "Stationary Sigma: " << endl;
-    stationarySigma().print();
+  cout << "Stationary: " << endl;
+  stationaryMean().print();
+  cout << "Stationary Sigma: " << endl;
+  stationarySigma().print();
 }
 
 /*
  * Markov Chain
  */
-MarkovChain::MarkovChain(const AR process, uword sz, double pmSd)
+MarkovChain::MarkovChain(const AR process, const uword sz, const double pmSd)
 {
-    double pMean = process.stationaryMean();
-    double pSd = process.stationarySigma();
-    rowvec support = linspace<rowvec>(pMean - pmSd * pSd, pMean + pmSd * pSd, sz);
+  double pMean = process.stationaryMean();
+  double pSd = process.stationarySigma();
+  rowvec support = linspace<rowvec>(pMean - pmSd * pSd, pMean + pmSd * pSd, sz);
 
-    mat transition(sz, sz);
-    for (uword rIx = 0; rIx < sz; ++rIx) {
-        double condMean = process.intercept() + process.rho() * support(rIx);
-        vec condPrs = discretizeNormal(support.t(), condMean, process.sigma());
-        transition.row(rIx) = condPrs.t();
-    }
+  mat transition(sz, sz);
+  for (uword rIx = 0; rIx < sz; ++rIx) {
+    double condMean = process.intercept() + process.rho() * support(rIx);
+    vec condPrs = discretizeNormal(support.t(), condMean, process.sigma());
+    transition.row(rIx) = condPrs.t();
+  }
 
-    MarkovChain(support, transition);
+  MarkovChain(support, transition);
 }
 
 const rowvec& MarkovChain::stationary()
 {
-    if (m_stationary.is_empty()) {
-        cx_vec eigval;
-        cx_mat eigvec;
-        eig_gen(eigval, eigvec, m_tran.t());
+  if (m_stationary.is_empty()) {
+    cx_vec eigval;
+    cx_mat eigvec;
+    eig_gen(eigval, eigvec, m_tran.t());
 
-        uword unitEigIx = (abs(abs(eigval) - 1.0)).index_min();
+    uword unitEigIx = (abs(abs(eigval) - 1.0)).index_min();
 
-        // cout << " sss " << unitEigIx << endl;
+    m_stationary = abs(eigvec.col(unitEigIx)).t();
+    m_stationary = m_stationary / sum(m_stationary);
+  }
 
-        m_stationary = abs(eigvec.col(unitEigIx)).t();
-        m_stationary = m_stationary / sum(m_stationary);
-    }
-
-    return m_stationary;
+  return m_stationary;
 }
 
-void MarkovChain::save(std::string fname) const
+void MarkovChain::save(const std::string fname) const
 {
-    m_support.save(hdf5_name(fname, "support", hdf5_opts::replace));
-    m_stationary.save(hdf5_name(fname, "stationary", hdf5_opts::replace));
-    m_tran.save(hdf5_name(fname, "transitions", hdf5_opts::replace));
+  m_support.save(hdf5_name(fname, "MC/support", hdf5_opts::replace));
+  m_stationary.save(hdf5_name(fname, "MC/stationary", hdf5_opts::replace));
+  m_tran.save(hdf5_name(fname, "MC/transitions", hdf5_opts::replace));
 }
 
 /*
  * Discretized VAR(1) => MarkovChain
  */
-void DiscreteVAR::save(std::string fname) const
+void DiscreteVAR::save(const std::string fname) const
 {
-    m_grids.save(hdf5_name(fname, "grids", hdf5_opts::replace));
-    m_mc.transition().save(hdf5_name(fname, "transitions", hdf5_opts::replace));
-    m_var.intercept().save(hdf5_name(fname, "varIntercept", hdf5_opts::replace));
-    m_var.rho().save(hdf5_name(fname, "varRho", hdf5_opts::replace));
-    m_var.sigma().save(hdf5_name(fname, "varSigma", hdf5_opts::replace));
+  m_grids.save(hdf5_name(fname, "DVAR/grids", hdf5_opts::replace));
+  m_mc.transition().save(hdf5_name(fname, "DVAR/transitions", hdf5_opts::replace));
+  m_var.intercept().save(hdf5_name(fname, "DVAR/varIntercept", hdf5_opts::replace));
+  m_var.rho().save(hdf5_name(fname, "DVAR/varRho", hdf5_opts::replace));
+  m_var.sigma().save(hdf5_name(fname, "DVAR/varSigma", hdf5_opts::replace));
 }
 
 void DiscreteVAR::impl(bool trimGrids)
 {
-    // Sizing
-    m_size = m_var.size();
-    m_flatSize = pow(m_supportSize, m_size);
-    m_grids.set_size(m_flatSize, m_size);
+  // Sizing
+  m_size = m_var.size();
+  m_flatSize = pow(m_supportSize, m_size);
+  m_grids.set_size(m_flatSize, m_size);
 
-    umat m_map(m_flatSize, m_size);
-    field<vec> m_orthogGrids(m_size);
+  umat m_map(m_flatSize, m_size);
+  field<vec> m_orthogGrids(m_size);
 
-    const mat mEye = eye(m_size, m_size);
+  const mat mEye = eye(m_size, m_size);
 
-    const mat AA = m_var.intercept();
-    const mat BB = m_var.rho();
-    const mat GG = m_var.sigma();
+  const mat AA = m_var.intercept();
+  const mat BB = m_var.rho();
+  const mat GG = m_var.sigma();
 
-    mat LL, VV;
-    vec DD;
-    svd(LL, DD, VV, GG);
+  mat LL, VV;
+  vec DD;
+  svd(LL, DD, VV, GG);
 
-    // Unconditional expectation and covariance matrix
-    const mat Atilde = LL.t() * AA;
-    const mat Btilde = LL.t() * BB * LL;
-    const mat diagDD = diagmat(DD);
+  // Unconditional expectation and covariance matrix
+  const mat Atilde = LL.t() * AA;
+  const mat Btilde = LL.t() * BB * LL;
+  const mat diagDD = diagmat(DD);
 
-    VAR orthog(Atilde, Btilde, diagDD);
-    const vec uncondE = orthog.stationaryMean();
-    const vec uncondV = orthog.stationarySigma().diag();
+  VAR orthog(Atilde, Btilde, diagDD);
+  const vec uncondE = orthog.stationaryMean();
+  const vec uncondV = orthog.stationarySigma().diag();
 
-    cout << "Original VAR: " << (m_var.stationaryQ() ? "Stationary" : "Nonstationary") << endl;
-    m_var.print();
-    cout << endl
-         << "Orthog VAR: " << (orthog.stationaryQ() ? "Stationary" : "Nonstationary") << endl;
-    orthog.print();
+  // cout << "Original VAR: " << (m_var.stationaryQ() ? "Stationary" : "Nonstationary") << endl;
+  // m_var.print();
 
-    // Prepare logical grids
+  // Prepare logical grids
+  for (uword vIx = 0; vIx < m_size; ++vIx) {
+    m_orthogGrids(vIx) = linspace<vec>(uncondE(vIx) - pmSd * sqrt(uncondV(vIx)),
+        uncondE(vIx) + pmSd * sqrt(uncondV(vIx)), m_supportSize);
+  }
+
+  // Prepare flat grids
+  for (uword flatIx = 0; flatIx < m_flatSize; ++flatIx) {
+    uword tmp = flatIx;
     for (uword vIx = 0; vIx < m_size; ++vIx) {
-        m_orthogGrids(vIx) = linspace<vec>(uncondE(vIx) - pmSd * sqrt(uncondV(vIx)),
-            uncondE(vIx) + pmSd * sqrt(uncondV(vIx)), m_supportSize);
+      const vec logical = m_orthogGrids(vIx);
+      m_map(flatIx, vIx) = tmp % m_supportSize;
+      m_grids(flatIx, vIx) = logical(m_map(flatIx, vIx));
+      tmp = tmp / m_supportSize;
     }
+  }
 
-    // Prepare flat grids
-    for (uword flatIx = 0; flatIx < m_flatSize; ++flatIx) {
-        uword tmp = flatIx;
-        for (uword vIx = 0; vIx < m_size; ++vIx) {
-            const vec logical = m_orthogGrids(vIx);
-            m_map(flatIx, vIx) = tmp % m_supportSize;
-            m_grids(flatIx, vIx) = logical(m_map(flatIx, vIx));
-            tmp = tmp / m_supportSize;
-        }
+  // Transition matrix
+  mat& tran = m_mc.transition();
+  tran.set_size(m_flatSize, m_flatSize);
+  for (uword flatIx = 0; flatIx < m_flatSize; ++flatIx) {
+    rowvec tmpVal = m_grids.row(flatIx);
+    vec condMeans = Atilde + Btilde * tmpVal.t();
+    vec condSd = sqrt(DD);
+    field<vec> condPrs(m_size);
+    for (uword vIx = 0; vIx < m_size; ++vIx)
+      condPrs(vIx) = discretizeNormal(m_orthogGrids(vIx), condMeans(vIx), condSd(vIx));
+
+    for (uword flatPrIx = 0; flatPrIx < m_flatSize; ++flatPrIx) {
+      double goPr = 1.0;
+      for (uword vPrIx = 0; vPrIx < m_size; ++vPrIx) {
+        vec tmpDist = condPrs(vPrIx);
+        goPr *= tmpDist(m_map(flatPrIx, vPrIx));
+      }
+      tran(flatIx, flatPrIx) = goPr;
     }
+  }
 
-    // Transition matrix
-    mat& tran = m_mc.transition();
-    tran.set_size(m_flatSize, m_flatSize);
-    for (uword flatIx = 0; flatIx < m_flatSize; ++flatIx) {
-        rowvec tmpVal = m_grids.row(flatIx);
-        vec condMeans = Atilde + Btilde * tmpVal.t();
-        vec condSd = sqrt(DD);
-        field<vec> condPrs(m_size);
-        for (uword vIx = 0; vIx < m_size; ++vIx)
-            condPrs(vIx) = discretizeNormal(m_orthogGrids(vIx), condMeans(vIx), condSd(vIx));
+  // Adjust grids
+  m_grids = (LL * m_grids.t()).t();
+  cout << "Completed discretization." << endl;
 
-        for (uword flatPrIx = 0; flatPrIx < m_flatSize; ++flatPrIx) {
-            double goPr = 1.0;
-            for (uword vPrIx = 0; vPrIx < m_size; ++vPrIx) {
-                vec tmpDist = condPrs(vPrIx);
-                goPr *= tmpDist(m_map(flatPrIx, vPrIx));
-            }
-            tran(flatIx, flatPrIx) = goPr;
-        }
-    }
+  if (trimGrids) {
+    /*
+     * Based on Gordon 2020 wip.
+     */
+    rowvec probs = m_mc.stationary();
+    uvec removePoints;
+    uword removeCount = 0;
+    for (uword flatIx = 0; flatIx < m_flatSize; ++flatIx)
+      if (probs(flatIx) <= minPr) {
+        removePoints.resize(removeCount + 1);
+        removePoints(removeCount) = flatIx;
+        removeCount++;
+      }
 
-    // Adjust grids
-    m_grids = (LL * m_grids.t()).t();
-    cout << "Completed discretization." << endl;
+    uword newFlatSize = m_flatSize - removeCount;
+    cout << "Removing " << removeCount << " points out of " << m_flatSize << "." << endl;
 
-    if (trimGrids) {
-        /*
-       * Based on Gordon 2020 wip.
-       */
-        rowvec probs = m_mc.stationary();
-        uvec removePoints;
-        uword removeCount = 0;
-        for (uword flatIx = 0; flatIx < m_flatSize; ++flatIx)
-            if (probs(flatIx) <= minPr) {
-                removePoints.resize(removeCount + 1);
-                removePoints(removeCount) = flatIx;
-                removeCount++;
-            }
+    mat newGrids = m_grids;
+    newGrids.shed_rows(removePoints);
+    mat newTran = tran;
+    newTran.shed_rows(removePoints);
+    newTran.shed_cols(removePoints);
+    for (uword fIx = 0; fIx < newFlatSize; ++fIx)
+      newTran.row(fIx) = newTran.row(fIx) / sum(newTran.row(fIx));
 
-        uword newFlatSize = m_flatSize - removeCount;
-        cout << "Removing " << removeCount << " points out of " << m_flatSize << "." << endl;
+    m_grids = newGrids;
+    tran = newTran;
+    m_flatSize = newFlatSize;
+  }
 
-        mat newGrids = m_grids;
-        newGrids.shed_rows(removePoints);
-        mat newTran = tran;
-        newTran.shed_rows(removePoints);
-        newTran.shed_cols(removePoints);
-        for (uword fIx = 0; fIx < newFlatSize; ++fIx)
-            newTran.row(fIx) = newTran.row(fIx) / sum(newTran.row(fIx));
-
-        m_grids = newGrids;
-        tran = newTran;
-        m_flatSize = newFlatSize;
-    }
+  // Finding midIx
+  vec distances(m_flatSize);
+  for (uword flatIx = 0; flatIx < m_flatSize; ++flatIx) {
+    const vec tmp = uncondE - m_grids.row(flatIx);
+    distances(flatIx) = sum(tmp % tmp);
+  }
+  m_midIx = distances.index_min();
 }
 
 void DiscreteVAR::print() const
 {
-    cout << "Grid:" << endl;
-    m_grids.print();
-    cout << endl
-         << "Transition matrix: " << endl;
-    m_mc.transition().print();
+  cout << "Grid:" << endl;
+  m_grids.print();
+  cout << endl
+       << "Transition matrix: " << endl;
+  m_mc.transition().print();
 }
 
 } // namespace TimeSeries
--- TimeSeries.hpp
+++ TimeSeries.hpp
@@ -5,26 +5,28 @@ using namespace arma;
 
 namespace TimeSeries {
 
-constexpr double TStol = 1.0E-10;
+const double TStol = 1.0E-10;
+const double pmSd = 3.5;
+const double minPr = 1.0E-4;
 
 struct DiscreteRV {
-    vec support;
-    vec probabilities;
+  vec support;
+  vec probabilities;
 };
 
 struct MvNormal {
-    vec mean;
-    mat varCovar;
+  vec mean;
+  mat varCovar;
 };
 
 /*
  * Normal helpers
  */
-DiscreteRV discretizeNormal(double mean,
-    double sigma,
-    uword n_elem = 21,
-    double pmSigma = 3.0);
-vec discretizeNormal(vec grid, double mean, double sigma);
+DiscreteRV discretizeNormal(const double mean,
+    const double sigma,
+    const uword n_elem = 21,
+    const double pmSigma = pmSd, bool cutTails = true);
+vec discretizeNormal(vec grid, const double mean, const double sigma, bool cutTails = true);
 
 /*
  *
@@ -32,28 +34,28 @@ vec discretizeNormal(vec grid, double mean, double sigma);
  *
  */
 class AR {
-public:
-    double intercept() const { return m_intercept; }
-    double rho() const { return m_rho; }
-    double sigma() const { return m_sigma; }
-    double stationaryMean() const { return m_intercept / (1.0 - m_rho); }
-    double stationarySigma() const
-    {
-        return m_sigma / std::sqrt(1.0 - m_rho * m_rho);
-    }
-    bool stationaryQ() const { return std::abs(m_rho) < 1.0; }
+  public:
+  double intercept() const { return m_intercept; }
+  double rho() const { return m_rho; }
+  double sigma() const { return m_sigma; }
+  double stationaryMean() const { return m_intercept / (1.0 - m_rho); }
+  double stationarySigma() const
+  {
+    return m_sigma / std::sqrt(1.0 - m_rho * m_rho);
+  }
+  bool stationaryQ() const { return std::abs(m_rho) < 1.0; }
 
-    AR(double intercept, double rho, double sigma)
-        : m_intercept(intercept)
-        , m_rho(rho)
-        , m_sigma(sigma)
-    {
-    }
+  AR(double intercept, double rho, double sigma)
+      : m_intercept(intercept)
+      , m_rho(rho)
+      , m_sigma(sigma)
+  {
+  }
 
-private:
-    double m_intercept;
-    double m_rho;
-    double m_sigma;
+  private:
+  double m_intercept;
+  double m_rho;
+  double m_sigma;
 };
 
 /*
@@ -62,37 +64,37 @@ private:
  *
  */
 class VAR {
-public:
-    uword size() const { return m_size; }
-    const vec& intercept() const { return m_intercept; }
-    const mat& rho() const { return m_rho; }
-    const mat& sigma() const { return m_sigma; }
+  public:
+  uword size() const { return m_size; }
+  const vec& intercept() const { return m_intercept; }
+  const mat& rho() const { return m_rho; }
+  const mat& sigma() const { return m_sigma; }
 
-    const MvNormal conditional(vec& current) const;
-    vec& stationaryMean();
-    mat& stationarySigma();
-    bool stationaryQ();
-    void print();
+  const MvNormal conditional(vec& current) const;
+  vec& stationaryMean();
+  mat& stationarySigma();
+  bool stationaryQ();
+  void print();
 
-    VAR(vec intercept, mat rho, mat sigma)
-        : m_intercept(intercept)
-        , m_rho(rho)
-        , m_sigma(sigma)
-    {
-        m_size = intercept.n_elem;
-    }
+  VAR(vec intercept, mat rho, mat sigma)
+      : m_intercept(intercept)
+      , m_rho(rho)
+      , m_sigma(sigma)
+  {
+    m_size = intercept.n_elem;
+  }
 
-private:
-    uword m_size;
-    vec m_intercept;
-    mat m_rho;
-    mat m_sigma;
+  private:
+  uword m_size;
+  vec m_intercept;
+  mat m_rho;
+  mat m_sigma;
 
-    vec m_statMean;
-    mat m_statSigma;
-    bool is_stationary;
+  vec m_statMean;
+  mat m_statSigma;
+  bool is_stationary;
 
-    void findStationary();
+  void findStationary();
 };
 
 /*
@@ -101,29 +103,29 @@ private:
  *
  */
 class MarkovChain {
-public:
-    uword size() const { return m_size; }
-    rowvec& support() { return m_support; }
-    const rowvec& support() const { return m_support; }
-    mat& transition() { return m_tran; }
-    const mat& transition() const { return m_tran; }
-    const rowvec& stationary();
-    void save(std::string fname) const;
+  public:
+  uword size() const { return m_size; }
+  rowvec& support() { return m_support; }
+  const rowvec& support() const { return m_support; }
+  mat& transition() { return m_tran; }
+  const mat& transition() const { return m_tran; }
+  const rowvec& stationary();
+  void save(std::string fname) const;
 
-    MarkovChain() { }
-    MarkovChain(rowvec support, mat transition)
-        : m_support(support)
-        , m_tran(transition)
-    {
-        m_size = support.n_elem;
-    }
-    MarkovChain(const AR process, uword sz = 21, double pmSd = 2.0);
+  MarkovChain() { }
+  MarkovChain(rowvec support, mat transition)
+      : m_support(support)
+      , m_tran(transition)
+  {
+    m_size = support.n_elem;
+  }
+  MarkovChain(const AR process, const uword sz = 21, const double pmSd = 2.0);
 
-private:
-    uword m_size;
-    rowvec m_support;
-    rowvec m_stationary;
-    mat m_tran;
+  private:
+  uword m_size;
+  rowvec m_support;
+  rowvec m_stationary;
+  mat m_tran;
 };
 
 /*
@@ -132,35 +134,35 @@ private:
  *
  */
 class DiscreteVAR {
-public:
-    uword size() const { return m_size; }
-    uword flatSize() const { return m_flatSize; }
-    const MarkovChain& markovChain() const { return m_mc; }
-    const vec gridIx(uword varIx) const { return m_grids.col(varIx); }
-    const rowvec gridValues(uword ix) const { return m_grids.row(ix); }
-    void save(std::string fname) const;
-    void print() const;
+  public:
+  uword size() const { return m_size; }
+  uword flatSize() const { return m_flatSize; }
+  uword midIx() const { return m_midIx; }
+  const MarkovChain& markovChain() const { return m_mc; }
+  const vec grid(uword varIx) const { return m_grids.col(varIx); }
+  const rowvec gridValues(uword ix) const { return m_grids.row(ix); }
+  void save(std::string fname) const;
+  void print() const;
 
-    DiscreteVAR(VAR var, uword supportSize, bool trimGrids = true)
-        : m_var(var)
-        , m_supportSize(supportSize)
-    {
-        impl(trimGrids);
-    }
+  DiscreteVAR(const VAR var, const uword supportSize, bool trimGrids = true)
+      : m_var(var)
+      , m_supportSize(supportSize)
+  {
+    impl(trimGrids);
+  }
 
-private:
-    VAR m_var;
-    MarkovChain m_mc;
+  private:
+  VAR m_var;
+  MarkovChain m_mc;
 
-    uword m_supportSize;
-    uword m_size;
-    uword m_flatSize;
-    const double pmSd = 3.5;
-    const double minPr = 1.0e-6;
+  uword m_supportSize;
+  uword m_size;
+  uword m_flatSize;
+  uword m_midIx;
 
-    mat m_grids;
+  mat m_grids;
 
-    void impl(bool trimGrids);
+  void impl(bool trimGrids);
 };
 
 } // namespace TimeSeries
